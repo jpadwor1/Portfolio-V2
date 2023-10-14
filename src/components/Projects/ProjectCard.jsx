8078e1f326810d3f82fcdaff238991ddf9b56ea2
jpadwor1 <128070765+jpadwor1@users.noreply.github.com>
2023-10-13 19:35:45 -0700

    update formatting

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,57 +1,68 @@
-import React from 'react'
-import './ProjectCard.css'
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-import GitHubIcon from '../../assets/Icons/GitHub2.svg'
+import React from "react";
+import "./ProjectCard.css";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import GitHubIcon from "../../assets/Icons/GitHub2.svg";
 
 function ProjectCard(props) {
   return (
     <div className={`project-content-${props.className}`}>
-            <a id={`project-image-container-${props.className}`} href={props.ProjectLink} rel="noopener noreferrer" target="_blank">
-            <div className={`project-image-container`}>
-                <img className={`project-image`} src= {props.ProjectImage} alt={props.ProjectTitle} />
-            </div>
-            </a>
-
-            <div className={`project-wrapper-${props.className}`}>    
-                <h2 className="project-title">
-                    {props.ProjectTitle}
-                </h2>
-                <div className="project-summary">
-                    <p>
-                        {props.ProjectDescription}
-                    </p>
-                    <ul className={`project-tech-${props.className}`}>
-                        {/* <div className="external-project-link"> */}
-                        <li>
-                            <a href={props.ProjectRepo} rel="noopener noreferrer" target="_blank">
-                                <img src={GitHubIcon} alt="Link"/>
-                            </a>
-                        </li>
-                        <li>
-                            <a href={props.ProjectLink} rel="noopener noreferrer" target="_blank">
-                                <OpenInNewIcon 
-                                    sx={{
-                                        color: "#ffbf38",
-                                        height: "40px",
-                                        width: "40px",
-                                        '&:hover': {
-                                            opacity: 0.5,
-                                        },
-                                    }}
-                                />
-                            </a>
-                        </li>
-                        {/* </div> */}
-                     {props.ProjectTags.map((tags) => (
-                        <li className="project-tag">{tags}</li>
-                     ))}
-
-                        
-                    </ul>
-                </div>       
+      <a
+        id={`project-image-container-${props.className}`}
+        href={props.ProjectLink}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <div className={`project-image-container`}>
+          <img
+            className={`project-image`}
+            src={props.ProjectImage}
+            alt={props.ProjectTitle}
+          />
         </div>
+      </a>
+
+      <div className={`project-wrapper-${props.className}`}>
+        <h2 className="project-title">{props.ProjectTitle}</h2>
+        <div className="project-summary">
+          <p>{props.ProjectDescription}</p>
+          <ul className={`project-tech-${props.className}`}>
+            {/* <div className="external-project-link"> */}
+            <li>
+              <a
+                href={props.ProjectRepo}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <img src={GitHubIcon} alt="Link" />
+              </a>
+            </li>
+            <li>
+              <a
+                href={props.ProjectLink}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <OpenInNewIcon
+                  sx={{
+                    color: "#ffbf38",
+                    height: "40px",
+                    width: "40px",
+                    "&:hover": {
+                      opacity: 0.5,
+                    },
+                  }}
+                />
+              </a>
+            </li>
+            {/* </div> */}
+            {props.ProjectTags.map((tags) => (
+              <li className="project-tag">{tags}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,48 +1,47 @@
-import React from 'react'
-import './Projects.css'
-import ProjectCard from './ProjectCard'
-import SelfTaughtDevImage from '../../assets/Images/SelfTaughDev.png'
-import Hydroponics from '../../assets/Images/Hydroponics.png'
-import TrailHead from '../../assets/Images/trailHead.png'
+import React from "react";
+import "./Projects.css";
+import ProjectCard from "./ProjectCard";
+import SelfTaughtDevImage from "../../assets/Images/SelfTaughDev.png";
+import Hydroponics from "../../assets/Images/Hydroponics.png";
+import TrailHead from "../../assets/Images/trailHead.png";
 function Projects() {
   return (
-    <section id='Projects' className="project-section">
-        <h2 className="project-heading">FEATURED PROJECTS</h2>
-        
-        <ProjectCard 
-            className="right"
-            ProjectLink="https://spectacular-gingersnap-720705.netlify.app" 
-            ProjectRepo="https://github.com/jpadwor1/TrailHead-App"
-            ProjectImage={TrailHead} 
-            ProjectTitle="TrailHead.io" 
-            ProjectDescription="TrailHead is a platform designed to help outdoor enthusiasts discover and explore trails for various activities such as hiking, mountain biking, running, and more. The website provides detailed information about trails, including reviews, ratings, and user experiences, allowing users to pick the right trail for their adventure."
-            ProjectTags={["ReactJS", "NodeJS", "ExpressJS", "MongoDB", "REST API"]}
-            /> 
+    <section id="Projects" className="project-section">
+      <h2 className="project-heading">FEATURED PROJECTS</h2>
 
-        <ProjectCard 
-            className="left"
-            ProjectLink="https://www.instructables.com/Cylindrical-Aeroponics-Growth-System/" 
-            ProjectRepo="https://github.com/jpadwor1/Simple-Projects/tree/main/selftaught-static"
-            ProjectImage={Hydroponics} 
-            ProjectTitle="Hydroponics Growth System" 
-            ProjectDescription="As a contestant in the 'Growing Beyond Earth' competition in partnership with NASA and Fairchild Tropical Botonical Garden. I designed and built a cylindrical system using basic electrical components controlled with C++. I was 2nd place overall and winner of the collegiate division. 
+      <ProjectCard
+        className="right"
+        ProjectLink="https://spectacular-gingersnap-720705.netlify.app"
+        ProjectRepo="https://github.com/jpadwor1/TrailHead-App"
+        ProjectImage={TrailHead}
+        ProjectTitle="TrailHead.io"
+        ProjectDescription="TrailHead is a platform designed to help outdoor enthusiasts discover and explore trails for various activities such as hiking, mountain biking, running, and more. The website provides detailed information about trails, including reviews, ratings, and user experiences, allowing users to pick the right trail for their adventure."
+        ProjectTags={["ReactJS", "NodeJS", "ExpressJS", "MongoDB", "REST API"]}
+      />
+
+      <ProjectCard
+        className="left"
+        ProjectLink="https://www.instructables.com/Cylindrical-Aeroponics-Growth-System/"
+        ProjectRepo="https://github.com/jpadwor1/Simple-Projects/tree/main/selftaught-static"
+        ProjectImage={Hydroponics}
+        ProjectTitle="Hydroponics Growth System"
+        ProjectDescription="As a contestant in the 'Growing Beyond Earth' competition in partnership with NASA and Fairchild Tropical Botonical Garden. I designed and built a cylindrical system using basic electrical components controlled with C++. I was 2nd place overall and winner of the collegiate division. 
             "
-            ProjectTags={["C++", "Arduino IDE", "HTML", "CSS"]}
-            /> 
+        ProjectTags={["C++", "Arduino IDE", "HTML", "CSS"]}
+      />
 
-        <ProjectCard 
-            className="right"
-            ProjectLink="https://jpadwor1.github.io/selftaughtdev/" 
-            ProjectRepo="https://github.com/jpadwor1/Simple-Projects/tree/main/selftaught-static"
-            ProjectImage={SelfTaughtDevImage} 
-            ProjectTitle="SelfTaughtDev/" 
-            ProjectDescription="A website I created to cut through the junk online courses for web development. A simple guide for self-taught developers to find courses from all around the web
+      <ProjectCard
+        className="right"
+        ProjectLink="https://jpadwor1.github.io/selftaughtdev/"
+        ProjectRepo="https://github.com/jpadwor1/Simple-Projects/tree/main/selftaught-static"
+        ProjectImage={SelfTaughtDevImage}
+        ProjectTitle="SelfTaughtDev/"
+        ProjectDescription="A website I created to cut through the junk online courses for web development. A simple guide for self-taught developers to find courses from all around the web
             that fit their needs."
-            ProjectTags={["HTML", "CSS", "BootStrap", "Javascript"]}
-            /> 
-        
+        ProjectTags={["HTML", "CSS", "BootStrap", "Javascript"]}
+      />
     </section>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
