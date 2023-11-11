fb0a513877591eaed8e7408704b9b424a5f422c6
jpadwor1 <128070765+jpadwor1@users.noreply.github.com>
2023-11-11 08:22:22 -0800

    add animation

--- src/components/SiteHeader/SiteHeader.jsx
+++ src/components/SiteHeader/SiteHeader.jsx
@@ -1,24 +1,59 @@
-import React from 'react'
-import './SiteHeader.css'
+import React from "react";
+import "./SiteHeader.css";
+import { motion } from "framer-motion";
 
 function SiteHeader() {
+
+   
+  const container = {
+    hidden: { opacity: 0,
+    y: '0.25em' },
+    show: {
+      opacity: 1,
+        y: 0,
+      transition: {
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0,y: '0.5em', scale: 1.05  },
+    show: { opacity: 1,y: 0, scale: 1 },
+    transition: {
+        type: "spring",
+        duration: 1,
+    }
+    };
+
   return (
     <section className="title-section">
-        <div className="title">
-            <h1 className="title-heading">Hi, my name is</h1>
-            <h2 className="title-subheading">John Padworski</h2>
-            <h3 className="title-tagline">
-                From concept to completion                 
-            </h3>
-            <h3 className="title-tagline">
-                I bring ideas to life
-            </h3>
-            <p className="title-body">
-                I am a software engineer and current student in Computer Science. I have a passion for turning ideas into reality from IoT to complex applications. 
-            </p>
-        </div>        
+      <motion.div
+        variants={container}
+        initial="hidden"
+        animate="show"
+        className="title"
+      >
+        <motion.h1 variants={item} className="title-heading">
+          Hi, my name is
+        </motion.h1>
+        <motion.h2 variants={item} className="title-subheading">
+          John Padworski
+        </motion.h2>
+        <motion.h3 variants={item} className="title-tagline">
+          From concept to completion
+        </motion.h3>
+        <motion.h3 variants={item} className="title-tagline">
+          I bring ideas to life
+        </motion.h3>
+        <motion.p variants={item} className="title-body">
+          I am a software engineer and current student in Computer Science. I
+          have a passion for turning ideas into reality from IoT to complex
+          applications.
+        </motion.p>
+      </motion.div>
     </section>
-  )
+  );
 }
 
-export default SiteHeader
+export default SiteHeader;
