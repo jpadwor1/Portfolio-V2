5af7893b4b8ca7f491a990002db365c775e5627f
jpadwor1 <128070765+jpadwor1@users.noreply.github.com>
2023-11-11 21:36:36 -0800

    update animations

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,51 +1,82 @@
-import React from 'react'
-import './About.css'
-import HeadShot from '../../assets/Images/headShot.jpg'
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import React from "react";
+import "./About.css";
+import HeadShot from "../../assets/Images/headShot.jpg";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import { motion } from "framer-motion";
+
+const animationVariants = {
+  initial: { opacity: 0, y: "100px" },
+  animate: (index) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: index * 0.25,
+      duration: 0.15,
+      type: "spring",
+    },
+  }),
+};
 
 function About() {
+  const techItems = ["Python", "HTML / CSS", "React", "NodeJS", "SQL", "NoSQL"];
   return (
     <section className="about-section">
-        <h2 id="about" className="about-heading">About Me</h2>
-        <div className="about-container">
-            <div className="about-description">
-              <p className="about-writeup">
-              Hello! My name is John Padworski, a military veteran turned software engineer with a passion for code. I'm driven by an insatiable appetite for solving intricate problems and crafting innovative solutions.<br/><br/>
-
-              Over the years, I've had the opportunity to work in diverse roles, from being a Frontend Web Developer at a non-profit "Know a Vet?" to a FinTech Startup and even roles in aviation. Each of these experiences has honed my unique problem-solving approach, which I bring to every project I undertake.<br/><br/>
-
-              Here are a few technologies and areas I've been deeply involved with recently:
-              </p>
-              <ul className="tech-list">
-                <li className="tech-list-item">
-                  <KeyboardArrowRightIcon sx={{
-                    color: "rgb(255, 178, 56)"
-                  }}
-                  /> Python</li>
-                <li className="tech-list-item"> <KeyboardArrowRightIcon sx={{
-                    color: "rgb(255, 178, 56)"
-                  }}/> HTML / CSS</li>
-                <li className="tech-list-item"> <KeyboardArrowRightIcon sx={{
-                    color: "rgb(255, 178, 56)"
-                  }}/> React</li>
-                <li className="tech-list-item"> <KeyboardArrowRightIcon sx={{
-                    color: "rgb(255, 178, 56)"
-                  }}/> NodeJS</li>
-                <li className="tech-list-item"> <KeyboardArrowRightIcon sx={{
-                    color: "rgb(255, 178, 56)"
-                  }}/> SQL</li>
-                <li className="tech-list-item"> <KeyboardArrowRightIcon sx={{
-                    color: "rgb(255, 178, 56)"
-                  }}/> NoSQL</li>
-              </ul>
-            </div>
-            <div className="photo">
-              <div className="photo-frame"></div>
-              <img src={HeadShot} alt="profile" className="about-image" />
-            </div>
+      <motion.h2
+        initial={{ opacity: 0, x: "-200px" }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        id="about"
+        className="about-heading"
+      >
+        About Me
+      </motion.h2>
+      <div className="about-container">
+        <div className="about-description">
+          <motion.p
+            initial={{ opacity: 0, y: "100px" }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.25, duration: 0.5 }}
+            className="about-writeup"
+          >
+            Hello! My name is John Padworski, a military veteran turned software
+            engineer with a passion for code. I'm driven by an insatiable
+            appetite for solving intricate problems and crafting innovative
+            solutions.
+            <br />
+            <br />
+            Over the years, I've had the opportunity to work in diverse roles,
+            from being a Frontend Web Developer at a non-profit "Know a Vet?" to
+            a FinTech Startup and even roles in aviation. Each of these
+            experiences has honed my unique problem-solving approach, which I
+            bring to every project I undertake.
+            <br />
+            <br />
+            Here are a few technologies and areas I've been deeply involved with
+            recently:
+          </motion.p>
+          <ul className="tech-list">
+            {techItems.map((item, index) => (
+              <motion.li
+                variants={animationVariants}
+                initial="initial"
+                animate="animate"
+                custom={index}
+                className="tech-list-item"
+                key={index}
+              >
+                <KeyboardArrowRightIcon sx={{ color: "rgb(255, 178, 56)" }} />{" "}
+                {item}
+              </motion.li>
+            ))}
+          </ul>
         </div>
+        <div className="photo">
+          <div className="photo-frame"></div>
+          <img src={HeadShot} alt="profile" className="about-image" />
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
